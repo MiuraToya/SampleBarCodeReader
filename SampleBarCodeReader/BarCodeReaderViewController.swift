46fc9d08b9a1b77a2b9b4d728814e98f5d6f0bc5
Miura Toya <dengzais8@gmail.com>
2021-02-24 15:47:03 +0900

    add

--- SampleBarCodeReader/BarCodeReaderViewController.swift
+++ SampleBarCodeReader/BarCodeReaderViewController.swift
@@ -1,110 +1,106 @@
-//
-//  BarCodeReaderViewController.swift
-//  SampleBarCodeReader
-//
-//  Created by 三浦　登哉 on 2021/02/22.
-//
+ 
 
-import Foundation
 import UIKit
 import AVFoundation
 
-final class BarCodeReaderViewController: UIViewController {
-    
+class BarCodeReaderVC: UIViewController {
+    // カメラやマイクの入出力を管理するオブジェクトを生成
     private let session = AVCaptureSession()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.view.backgroundColor = .white
-        
-        // カメラ、マイクを管理するオブジェクトを生成(デバイスの条件指定)
-        let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualWideCamera], mediaType: .video, position: .back)
-        
-        // 上記の条件に合致するデバイスを取得
+
+        // カメラやマイクのデバイスそのものを管理するオブジェクトを生成（ここではワイドアングルカメラ・ビデオ・背面カメラを指定）
+        let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera],
+            mediaType: .video,
+            position: .back)
+
+        // ワイドアングルカメラ・ビデオ・背面カメラに該当するデバイスを取得
         let devices = discoverySession.devices
-        
+
+        //　該当するデバイスのうち最初に取得したものを利用する
         if let backCamera = devices.first {
             do {
-                // デバイスをsessionに渡すための設定(カメラを取り込むイメージ)
+                // QRコードの読み取りに背面カメラの映像を利用するための設定
                 let deviceInput = try AVCaptureDeviceInput(device: backCamera)
-                
+                // デバイスを取り込む
                 if self.session.canAddInput(deviceInput) {
                     self.session.addInput(deviceInput)
-                    
-                    // 背面カメラの映像からQRコードを検出するための設定(データを取り込むイメージ)
+
+                    // 背面カメラの映像からQRコードを検出するための設定
                     let metadataOutput = AVCaptureMetadataOutput()
+                   // デバイスからの情報を取り込む
                     if self.session.canAddOutput(metadataOutput) {
                         self.session.addOutput(metadataOutput)
-                        
-                        // コードのデータ検出時の処理(第一引数はデリゲートの指定先 )
+
                         metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-                        // バーコードの種類を指定
+                        // 書籍のbar codeを設定
                         metadataOutput.metadataObjectTypes = [.ean13]
-                        
-                        // 検出可能範囲の設定
+
+                        // 読み取り可能エリアの設定を行う
+                        // 画面の横、縦に対して、左が10%、上が40%のところに、横幅80%、縦幅20%を読み取りエリアに設定
                         let x: CGFloat = 0.1
                         let y: CGFloat = 0.4
                         let width: CGFloat = 0.8
                         let height: CGFloat = 0.2
                         metadataOutput.rectOfInterest = CGRect(x: y, y: 1 - x - width, width: height, height: width)
-                        
-                        
-                        // 背面カメラの映像を画面に表示するためのレイヤーを作成
+
+                        // 背面カメラの映像を画面に表示するためのレイヤーを生成
                         let previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
-                                                previewLayer.frame = self.view.bounds
-                                                previewLayer.videoGravity = .resizeAspectFill
-                                                self.view.layer.addSublayer(previewLayer)
-                        
+                        previewLayer.frame = self.view.bounds
+                        // レイヤーいっぱいに表示
+                        previewLayer.videoGravity = .resizeAspectFill
+                        self.view.layer.addSublayer(previewLayer)
+
                         // 読み取り可能エリアに赤い枠を追加する
                         let detectionArea = UIView()
                         detectionArea.frame = CGRect(x: view.frame.size.width * x, y: view.frame.size.height * y, width: view.frame.size.width * width, height: view.frame.size.height * height)
                         detectionArea.layer.borderColor = UIColor.red.cgColor
                         detectionArea.layer.borderWidth = 3
                         view.addSubview(detectionArea)
-                        
+
                         // 閉じるボタン
                         let closeBtn:UIButton = UIButton()
                         closeBtn.frame = CGRect(x: 20, y: 20, width: 100, height: 40)
                         closeBtn.setTitle("閉じる", for: UIControl.State.normal)
                         closeBtn.backgroundColor = UIColor.lightGray
-                        closeBtn.addTarget(self, action: #selector(tappedBtn(sender:)), for: .touchUpInside)
-                        
-                        // 読み取る
+                        closeBtn.addTarget(self, action: #selector(closeTaped(sender:)), for: .touchUpInside)
+                        self.view.addSubview(closeBtn)
+
+                        // 読み取り開始
                         self.session.startRunning()
                     }
                 }
             } catch {
-                print(error.localizedDescription)
+                print("DEBUG_PRINT: エラーが発生")
             }
         }
-        
     }
+
+
     
-    // カメラの画質を設定
-    func setupCaptureSession() {
-        session.sessionPreset = AVCaptureSession.Preset.photo
-    }
-    
-    // 閉じるボタンの処理
-    @objc func tappedBtn(sender: UIButton) {
+    // 閉じるが押されたら呼ばれます
+    @objc func closeTaped(sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
 }
 
-
-extension BarCodeReaderViewController: AVCaptureMetadataOutputObjectsDelegate {
-    // 取得したデータの具体的な処理
+ extension BarCodeReaderVC: AVCaptureMetadataOutputObjectsDelegate {
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-        for metadata in metadataObjects as! [AVMetadataMachineReadableCodeObject] {
-            if metadata == nil {
-                return
-            } else {
-                let alert = UIAlertController(title: "バーコードの中身", message: metadata.stringValue, preferredStyle: .alert)
-                let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-                alert.addAction(OKAction)
+            for metadata in metadataObjects as! [AVMetadataMachineReadableCodeObject] {
+                // バーコードの内容が空かどうかの確認
+                if metadata.stringValue == nil { continue }
+
+                // 読み取ったデータの値
+                print(metadata.type)
+                print(metadata.stringValue!)
+                
+                // 取得したデータの処理を行う
+                let alert: UIAlertController = UIAlertController(title: "バーコード", message: metadata.stringValue, preferredStyle: UIAlertController.Style.alert)
+                let cancel: UIAlertAction = UIAlertAction(title: "キャンセル", style: UIAlertAction.Style.cancel, handler:nil)
+                alert.addAction(cancel)
                 present(alert, animated: true, completion: nil)
             }
         }
-    }
-}
+ }
--- SampleBarCodeReader/ViewController.swift
+++ SampleBarCodeReader/ViewController.swift
@@ -9,15 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
-        override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
         
+        // バーコードリーダーのモーダルが開くボタン
+        let buttonSize: CGFloat = 100
+        let button: UIButton = UIButton()
+        button.frame = CGRect(x: (self.view.frame.width - buttonSize) / 2, y: (self.view.frame.height - buttonSize) / 2, width: buttonSize, height: buttonSize)
+        button.backgroundColor = .red
+        button.setTitle("開く", for: UIControl.State.normal)
+        button.addTarget(self, action: #selector(taped(sender:)), for: .touchUpInside)
+        self.view.addSubview(button)
     }
     
-    
-
-    @IBAction func BarCodeReader(_ sender: Any) {
-        self.present(BarCodeReaderViewController(), animated: true, completion: nil)
+    // ボタンが押されたら呼ばれます
+    @objc func taped(sender: UIButton) {
+        self.present(BarCodeReaderVC(), animated: true, completion: nil)
     }
 }
-
